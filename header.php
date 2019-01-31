5b1a4df690e86883d76a9e3341e26e909458b198
Augusto-DF <dragonflycorp@outlook.com>
2019-01-31 18:06:42 -0300

    Layout da primeira tela versao mobile finalizada

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Belíssima Esmalteria</title>
+</head>
+<style>
+	@import url('https://fonts.googleapis.com/css?family=Josefin+Slab:300,400,600');
+	@import url('https://fonts.googleapis.com/css?family=Niconne:400,600');
+</style>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/grid.css">
+<link rel="stylesheet" href="css/style.css">
+
+
+<body>
+	<header class="menu">
+		<div class="menu-sandwich">
+			<a href="/"><img src="img/header/menu-sandwich.png"></a>
+		</div>
+		<div class="container">
+			<div class="logo">
+				<a href="/"><img src="img/header/logo.png"></a>
+			</div>
+		</div>		
+	</header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	include 'header.php';
+?> 
+<!-- <body> -->
+<!-- Introdução -->	
+	<section class="intro-bg">
+		<div class="filter">
+			<div class="container">
+				<div class="intro">
+					<h1>Especializada em deixar você</h1>
+					<h2>Belíssima</h2>
+				</div>
+			</div>
+		</div>		
+	</section>
+
+
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+</body>
+
+
+</html>
