31bb6ff7080a4cc87b2861ae8832bf3d9f6d3e38
Augusto-DF <dragonflycorp@outlook.com>
2019-02-01 02:08:08 -0300

    Layout home sessao de transiçao finalizada

--- index.php
+++ index.php
@@ -14,6 +14,58 @@
 		</div>		
 	</section>
 
+<!-- Sessão de Serviços -->
+	<section class="second-sc">
+		<div class="container">
+			<!-- Ancora unha -->
+			<a href="unha.php">
+				<div class="box-service" id="unha">
+					<div class="svc-name">
+						<h3>Unhas</h3>
+					</div>
+				</div>
+			</a>
+			<!-- Ancora cabelo -->
+			<a href="cabelo.php">
+				<div class="box-service" id="cabelo">
+					<div class="svc-name">
+						<h3>Cabelo</h3>
+					</div>
+				</div>
+			</a>
+			<!-- Ancora depilacao -->
+			<a href="depilacao.php">
+				<div class="box-service" id="depilacao">
+					<div class="svc-name">
+						<h3>Depilacao</h3>
+					</div>
+				</div>
+			</a>
+			<!-- Ancora make -->
+			<a href="make.php">
+				<div class="box-service" id="make">
+					<div class="svc-name">
+						<h3>Maquiagem</h3>
+					</div>
+				</div>
+			</a>
+		</div>
+	</section>
+
+<!-- Sessão de transição -->
+	<div class="third-sc">
+		<div class="container">
+			<a href=""><div class="agen">
+				<h3>Agende um horário</h3>
+			</div></a>
+			<img src="img/home/logo-transit.png">
+			<div class="cite">
+				<h2>"A beleza começa no momento em que você decide ser você mesma"</h2>
+				<div class="hor-bar"></div>
+				<blockquote>Coco Chanel</blockquote>
+			</div>			
+		</div>
+	</div>	
 
 	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 </body>
