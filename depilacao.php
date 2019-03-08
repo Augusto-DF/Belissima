48721f06c0f5aa7bd01fcee2bfc4bec8970f4173
Augusto-DF <dragonflycorp@outlook.com>
2019-03-08 14:51:21 -0300

    Layout depilacao desktop finalizado

--- depilacao.php
+++ depilacao.php
@@ -16,23 +16,42 @@
 <!-- Procedimentos -->
 	<section class="procedures">
 		<div class="container">
-			<ul>
-				<h2>Depilação</h2>
-				
-				<li>Design de sobrancelha</li>
-				<li>Sobrancelha na linha</li>
-				<li>Sobrancelha com henna</li>
-				<li>Buço na cera</li>
-				<li>Buço na linha</li>
-				<li>Depilação facial</li>
-				<li>Axila</li>
-				<li>Meia perna</li>
-				<li>Perna inteira</li>
-				<li>Coxa</li>
-				<li>Braço</li>
-				<li>Ignal comum ( cantos )</li>
-				<li>Ignal profunda</li>
-			</ul>
+			<div>			
+				<ul>
+					<h3>Depilação</h3>
+					
+					<li>Design de sobrancelha</li>
+					<li>Sobrancelha na linha</li>
+					<li>Sobrancelha com henna</li>
+					<li>Buço na cera</li>
+					<li>Buço na linha</li>
+					<li>Depilação facial</li>
+					<li>Axila</li>
+					<li>Meia perna</li>
+					<li>Perna inteira</li>
+					<li>Coxa</li>
+					<li>Braço</li>
+					<li>Ignal comum ( cantos )</li>
+					<li>Ignal profunda</li>
+				</ul>
+			</div>
+			<div class="vertical-bar-xl invisible-mb"></div>
+			<div class="invisible-mb">
+				<ul>
+					<h3>Horários</h3>
+
+					<li>Segunda a Sabado: 9h - 18h</li>
+					<li>O salão não abre aos domingos</li>
+
+					<h3 class="break-point">Agende um horário</h3>	
+
+					<p>Ligue agora e agende um horário no </br> 
+					Salão Belissima:</p>
+					<h4>(84) 3207-1599</h4>
+					<h4>(84) 98805-0464 ( Whatsapp )</h4>
+					<h4>Av. dos Ipês, 754 A - Neópolis - </br>Natal - RN</h4>
+				</ul>				
+			</div>
 		</div>
 	</section>
 <!-- Serviços -->
