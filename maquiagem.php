f08152293d299f10d96a38bd3b63e298f12fd7fb
Augusto-DF <dragonflycorp@outlook.com>
2019-03-09 13:54:48 -0300

    Layout desktop maquiagem finalizado

--- maquiagem.php
+++ maquiagem.php
@@ -16,13 +16,32 @@
 <!-- Procedimentos -->
 	<section class="procedures">
 		<div class="container">
-			<ul>
-				<h2>Maquiagem</h2>
-				
-				<li>Sem cílios</li>
-				<li>Com cílios</li>
-				<li>Básica</li>
-			</ul>
+			<div>
+				<ul>
+					<h3>Maquiagem</h3>
+					
+					<li>Sem cílios</li>
+					<li>Com cílios</li>
+					<li>Básica</li>
+				</ul>
+			</div>			
+			<div class="vertical-bar-xl invisible-mb"></div>
+			<div class="invisible-mb">
+				<ul>
+					<h3>Horários</h3>
+
+					<li>Segunda a Sabado: 9h - 18h</li>
+					<li>O salão não abre aos domingos</li>
+
+					<h3 class="break-point">Agende um horário</h3>	
+
+					<p>Ligue agora e agende um horário no </br> 
+					Salão Belissima:</p>
+					<h4>(84) 3207-1599</h4>
+					<h4>(84) 98805-0464 ( Whatsapp )</h4>
+					<h4>Av. dos Ipês, 754 A - Neópolis - </br>Natal - RN</h4>
+				</ul>				
+			</div>
 		</div>
 	</section>
 <!-- Serviços -->
